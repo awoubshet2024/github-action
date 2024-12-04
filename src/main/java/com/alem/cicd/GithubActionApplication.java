6d09a62e382891d750f251f01fb0cf689e34b40c
alemtutor <alem.woubshet@gmail.com>
2024-12-04 00:18:10 -0500

    Jdk version changed from 17 to 21

--- src/main/java/com/alem/cicd/GithubActionApplication.java
+++ src/main/java/com/alem/cicd/GithubActionApplication.java
@@ -2,14 +2,18 @@ package com.alem.cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GithubActionApplication {
 
     public static void main(String[] args) {
 
         SpringApplication.run(GithubActionApplication.class, args);
     }
+    @GetMapping("/message")
     public String getMessage(){
         return "Hello World";
     }
